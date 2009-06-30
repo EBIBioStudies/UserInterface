2a978c08f9d97d65fcfbb81b6a4477a32ecf3bdd
kolais <kolais@2913f559-6b04-0410-9a09-c530ee9f5186>
2009-06-30 16:00:38 +0000

    moving project to subproject (part 4)

---
+++ shared-components/auth/src/main/java/uk/ac/ebi/microarray/arrayexpress/shared/auth/AuthenticationHelper.java
@@ -0,0 +1,5 @@
+package uk.ac.ebi.microarray.arrayexpress.shared.auth;
+
+public class AuthenticationHelper
+{
+}
---
+++ shared-components/auth/src/test/java/uk/ac/ebi/microarray/arrayexpress/shared/auth/AuthenticationHelperTest.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.microarray.arrayexpress.shared.auth;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthenticationHelperTest
+{
+    @Before
+    public void intializeApplication()
+    {
+    }
+
+    @Test
+    public void testVerifyHash()
+    {
+    }
+}
