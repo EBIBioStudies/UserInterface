9d5e129b93ff439fadd6ac86982c18b67ba655d5
kolais <kolais@2913f559-6b04-0410-9a09-c530ee9f5186>
2009-08-24 14:15:51 +0000

    cleanup 2

--- webapp/src/main/java/uk/ac/ebi/arrayexpress/utils/model/ExperimentList.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.arrayexpress.utils.model;
-
-import uk.ac.ebi.arrayexpress.model.ExperimentBean;
-
-import java.util.ArrayList;
-
-public class ExperimentList extends ArrayList<ExperimentBean> {
-    // nothing here yet
-}
--- webapp/src/main/java/uk/ac/ebi/arrayexpress/utils/model/parser/ExperimentParserMode.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.ebi.arrayexpress.utils.model.parser;
-
-public enum ExperimentParserMode {
-    SINGLE_EXPERIMENT,
-    MULTIPLE_EXPERIMENTS
-}
